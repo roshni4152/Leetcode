112588c2bf88d27660b2fba20836314190823ece
roshni4152 <97780497+roshni4152@users.noreply.github.com>
2022-05-02 22:18:54 +0530

    Time: 4 ms (24.89%), Space: 6.2 MB (96.28%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isValid(string s) {
+       vector<char>stk;
+        for(auto i:s){
+            if(stk.empty()) stk.push_back(i);
+            else if(stk.back()=='(' and i==')')
+                stk.pop_back();
+            else if(stk.back()=='{' and i=='}')
+                stk.pop_back();
+            else if(stk.back()=='[' and i==']')
+                stk.pop_back();
+            else stk.push_back(i);
+        }
+        if(stk.empty()) return true;
+        else return false;
+    }
+};
