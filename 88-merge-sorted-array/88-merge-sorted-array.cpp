d5067ca5596bb24c047e1f672c86b07010be13a5
roshni4152 <97780497+roshni4152@users.noreply.github.com>
2022-06-07 21:11:06 +0530

    Time: 3 ms (72.68%), Space: 9.1 MB (30.68%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -4,6 +4,6 @@ public:
         for(int i=0;i<n;i++){
             nums1[m+i]=nums2[i];
         }
-        sort(nums1.begin(),nums1.end());
+        sort(nums1.begin(),nums1.begin()+m+n);
     }
 };
