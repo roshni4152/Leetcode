3d3398a4f43156e796108f030276ea83170b4b03
roshni4152 <97780497+roshni4152@users.noreply.github.com>
2022-06-12 23:36:29 +0530

    Time: 10 ms (67.44%), Space: 10.9 MB (68.06%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+   void findcombo(int ind,int target,vector<int> &candidates,vector<vector<int>> &ans,vector<int>&ds){
+        if(ind==candidates.size()){
+            if(target==0) {
+                ans.push_back(ds);}
+        return;
+        }
+        if(candidates[ind]<=target){
+            ds.push_back(candidates[ind]);
+            findcombo(ind,target-candidates[ind],candidates,ans,ds);
+            ds.pop_back();
+        }
+        findcombo(ind+1,target,candidates,ans,ds);
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>>ans;
+        vector<int>ds;
+        findcombo(0,target,candidates,ans,ds);
+        return ans;
+    }
+};
