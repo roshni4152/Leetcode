6a4e4ac78197357cf1143e885f6e80f249487735
roshni4152 <97780497+roshni4152@users.noreply.github.com>
2022-06-20 19:37:40 +0530

    Time: 27 ms (10.54%), Space: 7.3 MB (45.58%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-       int const CHAR=256;
         if(s.length()!=t.length()) return false;
-        int count[CHAR]={0};
-        for(int i=0;i<s.size();i++){
-            count[s[i]]++;count[t[i]]--;
-        }
-        for(int i=0;i<CHAR;i++){
-            if(count[i]!=0) return false;
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        for(int i=0;i<s.length();i++){
+            if(s[i]!=t[i]) return false;
         }
         return true;
     }
