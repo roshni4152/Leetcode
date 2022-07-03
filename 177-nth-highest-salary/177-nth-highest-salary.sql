24111a8ae52ea7c1d96d87a27bee40ce6013ed4d
roshni4152 <97780497+roshni4152@users.noreply.github.com>
2022-07-03 10:19:25 +0530

    Time: 396 ms (50.05%), Space: 0B (100.00%) - LeetHub

--- 177-nth-highest-salary/177-nth-highest-salary.sql
+++ 177-nth-highest-salary/177-nth-highest-salary.sql
@@ -2,7 +2,10 @@ CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
 set N=N-1;
   RETURN (
-      select distinct salary from employee
-      order by salary desc limit 1 offset N
+      select 
+      distinct salary 
+      from employee
+      order by salary desc 
+      limit 1 offset N
   );
 END
