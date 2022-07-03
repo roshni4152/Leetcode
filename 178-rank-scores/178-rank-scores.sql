b644429caca0d8b93dc0b29ef7d9cb1895fb0a87
roshni4152 <97780497+roshni4152@users.noreply.github.com>
2022-07-03 10:57:19 +0530

    Time: 242 ms (90.92%), Space: 0B (100.00%) - LeetHub

--- 178-rank-scores/178-rank-scores.sql
+++ 178-rank-scores/178-rank-scores.sql
@@ -1,3 +1,6 @@
 # Write your MySQL query statement below
-select score, dense_rank() over ( order by score desc) as 'rank' from Scores order by score desc;
+select score, dense_rank() 
+over ( order by score desc) as 'rank' 
+from Scores
+order by score desc;
 
